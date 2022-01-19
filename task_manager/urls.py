d5daed743fd577f302c402bd03db7a8bc602582f
Krishna Sivakumar <krishnasivaprogrammer@gmail.com>
2022-01-19 23:29:26 +0530

    migrated views from in-memory data structures to ORM

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
 from django.urls import path
+import tasks.views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("tasks/", tasks.views.tasks_view),
+    path("add-task/", tasks.views.add_task_view),
+    path("delete-task/<int:task_index>/", tasks.views.delete_task_view),
+    path("complete_task/<int:task_index>/", tasks.views.complete_task_view),
+    path("completed_tasks/", tasks.views.completed_task_view),
+    path("all_tasks/", tasks.views.all_tasks_view),
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,7 +1,36 @@
-# Add your Views Here
+# Add all your views here
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from tasks.models import Task
 
 
 def tasks_view(request):
-    pass
+    return render(request, "current.html", {"tasks": Task.objects.filter(completed=False, deleted=False)})
+
+
+def add_task_view(request):
+    task_value = request.GET.get("task")
+    if task_value:
+        Task(title=task_value).save()
+    return HttpResponseRedirect("/tasks/")
+
+
+def delete_task_view(request, task_index):
+    Task.objects.filter(id=task_index).update(deleted=True)
+    return HttpResponseRedirect("/tasks/")
+
+
+def complete_task_view(request, task_index):
+    Task.objects.filter(id=task_index).update(completed=True)
+    return HttpResponseRedirect("/tasks/")
+
+
+def completed_task_view(request):
+    return render(request, "completed.html", {"tasks": Task.objects.filter(completed=True)})
+
+
+def all_tasks_view(request):
+    return render(
+        request, "all.html", {"current": Task.objects.filter(completed=False, deleted=False),
+                              "completed": Task.objects.filter(completed=True)}
+    )
